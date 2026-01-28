e456ee995f078c874d14a50efc8a444c6d359b75
MikhailOshchepkov <mikhail.oshchepkov.07@mail.ru>
2026-01-28 19:29:33 +0500

    init: processing error

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import express from "express";
 import healthRouter from "./routes/health.router"
+import { Server } from "node:http";
 
 config();
 
@@ -10,6 +11,39 @@ function buildApp() {
     app.use(express.json());
 
     app.use("/health", healthRouter);
+
+    app.use((req, res) => {
+        res.status(404).json(
+            {
+                status: 'Not found',
+            }
+        );
+    });
+
+
+    app.use((err: unknown, _req: express.Request, res: express.Response) => {
+
+        console.error(err);
+
+        if(err instanceof Error)
+        {
+            return res.status(500).json({
+                status: 'Internal Server Error',
+                message: err.message
+            });
+        } else if(typeof err === 'string')
+        {
+            return res.status(500).json({
+                status: 'Internal Server Error',
+                message: err,
+            });
+        }else {
+            res.status(500).json({
+                status: 'Error',
+                message: 'Internal Server Error',
+            });
+        }
+        });
 
     return app;
 }
