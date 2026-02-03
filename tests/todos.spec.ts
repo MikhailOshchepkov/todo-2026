03d713ecc8bfc26508fc174c48b00692ab537200
MikhailOshchepkov <mikhail.oshchepkov.07@mail.ru>
2026-02-03 16:49:44 +0500

    fix: update test

--- tests/todos.spec.ts
+++ tests/todos.spec.ts
@@ -1,23 +1,32 @@
 
-import { createTodo, listTodos } from "../src/services/todos.service";
+import { createTodo, listTodos, createCategory } from "../src/services/todos.service";
 
-describe ('todos service', () => {
+describe('todos service', () => {
 
-    test('list todos is empty', ()=> {
+    test('list todos is empty', () => {
         const all = listTodos();
         expect(all).toEqual([]);
     })
 
     test('create and list todos', () => {
-        const todo = createTodo('test');
+        // Сначала создаем категорию
+        const category = createCategory('Test Category');
+        // Затем создаем todo с categoryId
+        const todo = createTodo('test', category.id);
         const all = listTodos();
         expect(all).toEqual([todo]);
     })
 
-    test('Неправильное создание', ()=> {
-        //@ts-ignore
-        //const todo = createTodo();
-        
-        expect(() => createTodo()).toThrow('Title is required');
+    test('Неправильное создание - без title', () => {
+        const category = createCategory('Test Category 2');
+        expect(() => createTodo('', category.id)).toThrow('Title is required');
+    })
+
+    test('Неправильное создание - без categoryId', () => {
+        expect(() => createTodo('test title', 0)).toThrow('Valid category ID is required');
+    })
+
+    test('Неправильное создание - с несуществующим categoryId', () => {
+        expect(() => createTodo('test title', 999)).toThrow('Valid category ID is required');
     })
 })
